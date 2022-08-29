0c4634d4a060505e5093908ccc869a3f2508694f
Marco Silvestrin <marco.silvestrin97@gmail.com>
2022-08-29 17:13:51 +0200

    hello world with append

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HelloWorld
 {
@@ -6,7 +7,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File.AppendAllText(@"C:\repos\Greeting.txt", "Hello world!" + Environment.NewLine);
         }
     }
 }
